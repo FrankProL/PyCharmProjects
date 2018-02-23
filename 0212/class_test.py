36e56d05061edc3e0d7177d9aaf09936e4fe7c8d
Frank <ljffrank@163.com>
2018-02-23 09:11:13 +0800

    Signed-off-by: Frank <ljffrank@163.com>

--- 0212/class_test.py
+++ 0212/class_test.py
@@ -84,3 +84,78 @@ print c1.getID()
 print c1.name
 c1.name='lisi'
 print c1.name
+"""描述符 
+   实现__set__ __get__ __del__ 三个方法的类
+"""
+class Property(object):
+    def __init__(self,propname,datatype,default =None):
+        self._name='_'+propname+'_'
+        self._type=datatype
+        self._default = default if default else self._type()
+
+    def __get__(self, instance, owner):
+        return getattr(instance,self._name,self._default)
+
+    def __set__(self, instance, value):
+        if not isinstance(value,self._type):
+            raise TypeError('Type Error , must be %s type0' % self._type)
+        setattr(instance,self._name,value)
+
+    def __del__(self):
+        pass
+
+class Email(Property):
+    def __init__(self,propname,default=None):
+        super(Email,self).__init__(propname,str,default)
+
+    def __set__(self, instance, value):
+        if not isinstance(value,self._type):
+            raise TypeError('Type Error , must be %s type0' % self._type)
+        if not '@' in value :
+            raise ValueError('Email address is not valid')
+        setattr(instance,self._name,value)
+
+
+class Chinese(object):
+
+
+    ID = Property('id',int)
+    Name = Property('name',str)
+    Email = Email('email')
+
+    def __init__(self,id,name,email):
+        self.ID = id
+        self.Name=name
+        self.Email = email
+
+    @staticmethod
+    def getPeopleByParents(mather,father):
+        print mather,father
+        return Chinese(10,'dasheng','ab@def')
+
+    @classmethod
+    def getPeopleBySibling(cls,sibling):
+        print sibling
+        return cls(20,'bajie','de@ad')
+
+    def __str__(self):
+        return 'ID = {0} , Name = {1} ,Email = {2}'.format(self.ID,self.Name,self.Email)
+
+    def __repr__(self):
+        return 'ID={0}'.format(self.ID)
+
+    def __add__(self,other):
+        return self.ID + other.ID
+
+    # def __lt__(self, other):
+    #     return self.ID < other.ID
+
+class JiLin(Chinese):
+    pass
+
+
+dasheng = JiLin.getPeopleByParents('mather','father')
+bajie = JiLin.getPeopleBySibling('dasheng')
+
+print dasheng
+print bajie
