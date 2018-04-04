3edd6ac5e81f817cf930e906658a4b1a5d2a32fe
FrankProL <ljffrank1@gmail.com>
2018-04-04 19:02:16 +0800

    常用排序算法，python实现

---
+++ sort_test/heap_sort.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+# @Time    : 2018/4/4 14:57
+# @Author  : Frank
+# @Site    : 
+# @File    : heap_sort.py
+# @Software: PyCharm
+"""
+import random
+
+
+def buildMaxHeap(arr):
+    import math
+    for i in range(int(math.floor(len(arr) / 2)), -1, -1):
+        heapify(arr, i)
+
+
+def heapify(arr, i):    # 根节点为0，结点下标为i,左孩子则为2*i+1,右孩子下标则为2*i+2
+    left = 2 * i + 1
+    right = 2 * i + 2
+    largest = i
+    if left < arrLen and arr[left] > arr[largest]:
+        largest = left
+    if right < arrLen and arr[right] > arr[largest]:
+        largest = right
+
+    if largest != i:
+        swap(arr, i, largest)
+        heapify(arr, largest)
+        print arr
+
+
+def swap(arr, i, j):
+    arr[i], arr[j] = arr[j], arr[i]
+
+
+def heapSort(arr):
+    global arrLen
+    arrLen = len(arr)
+    buildMaxHeap(arr)
+    for i in range(len(arr) - 1, 0, -1):
+        swap(arr, 0, i)
+        arrLen -= 1
+        heapify(arr, 0)
+    return arr
+
+if __name__ == '__main__':
+    data=[random.randint(1,100) for i in range(10)]
+    print data
+    print heapSort(data)
