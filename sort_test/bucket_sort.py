14b1213afd4ae205b563b7669903a1b7eea73a6e
FrankProL <ljffrank1@gmail.com>
2018-04-10 17:23:59 +0800

    排序算法完善

--- sort_test/bucket_sort.py
+++ sort_test/bucket_sort.py
@@ -16,7 +16,7 @@ from sort_test.insert_sort import insert_sort
 """
     桶排序是计数排序的升级版。它利用了函数的映射关系，高效与否的关键就在于这个映射函数的确定。为了使桶排序更加高效，我们需要做到这两点：
         在额外空间充足的情况下，尽量增大桶的数量
-        使用的映射函数能够将输入的 N 个数据均匀的分配到 K 个桶中
+        使用的映射函数能够将输入的 N 个数据尽可能均匀的分配到 K 个桶中
     同时，对于桶中元素的排序，选择何种比较排序算法对于性能的影响至关重要。
 
     桶排序类似于计数排序，当桶的数量大于max-min时，就相当于计数排序
@@ -32,16 +32,20 @@ def bucket_sort(arr, bucketnum):       # bucketnum 桶个数
             minvalue = arr[i]       # 输入数据的最小值
         elif (arr[i] > maxvalue):
             maxvalue = arr[i]       # 输入数据的最大值
+    if maxvalue==minvalue: return arr   # 最大值等于最小值，即单一值，不用排序，主要用于递归调用时，最终桶内只剩下相等元素
     bucketspan = int(math.floor((maxvalue - minvalue) / bucketnum + 1))      # 相当于桶的跨度区间
     buckets = [[] for i in range(bucketnum)]                                  # 初始化bucket个空桶
 
     for i in range(0, len(arr)):                                                # 利用映射函数将数据分配到各个桶中
         buckets[int(math.floor((arr[i] - minvalue) / bucketspan))].append(arr[i])
+    print buckets
 
 
     arr = []
     for i in range(0, len(buckets)):
-        insert_sort(buckets[i])                         # 对每个桶进行排序，这里调用了插入插入排序算法
+        # insert_sort(buckets[i])                         # 对每个桶内进行排序，这里调用了插入排序算法
+        if len(buckets[i])>1:                             # 递归调用桶排序：当桶内元素大于1时，对桶内元素继续按桶排序
+            bucket_sort(buckets[i],10)
         for j in range(0, len(buckets[i])):
             arr.append(buckets[i][j])
     return arr
--- sort_test/radix_sort.py
+++ sort_test/radix_sort.py
@@ -7,10 +7,14 @@
 # @File    : radix_sort.py
 # @Software: PyCharm
 """
+import random
+
 """基数排序是一种非比较型整数排序算法，
     其原理是将整数按位数切割成不同的数字，然后按每个位数分别比较。
     由于整数也可以表达字符串（比如名字或日期）和特定格式的浮点数，所以基数排序也不是只能使用于整数。
     基数排序有两种方法：
+        LSD（Least significant digital）或MSD（Most significant digital），
+        LSD的排序方式由键值的最右边开始，而MSD则相反，由键值的最左边开始
     
     基数排序 vs 计数排序 vs 桶排序
         这三种排序算法都利用了桶的概念，但对桶的使用方法上有明显差异：
@@ -18,3 +22,49 @@
             计数排序：每个桶只存储单一键值；
             桶排序：每个桶存储一定范围的数值；
 """
+
+
+def radix_sort(arr):
+    maxsize = len(str(max(arr)))
+    for k in xrange(maxsize):  # k轮排序， 最多几位数，就进行几轮桶排序
+        bucket = [[] for i in xrange(10)]
+        for i in arr:
+            bucket[i / (10 ** k) % 10].append(i)  # 析取整数第K位数字 （从低到高）
+        arr = [a for b in bucket for a in b]  # 直到最大数的最高位也被添加到桶中，或者说，当所有的元素都被被在第 0 个桶中，基数排序就结束了
+    return arr
+
+
+# def sort(a, radix=10):
+#     """a为整数列表， radix为基数"""
+#     K = int(math.ceil(math.log(max(a), radix)))  # 用K位数可表示任意整数
+#     bucket = [[] for i in range(radix)]  # 不能用 [[]]*radix
+#     for i in range(1, K + 1):  # K次循环
+#         for val in a:
+#             bucket[val % (radix ** i) / (radix ** (i - 1))].append(val)  # 析取整数第K位数字 （从低到高）
+#         del a[:]
+#         for each in bucket:
+#             a.extend(each)  # 桶合并
+#         bucket = [[] for i in range(radix)]
+
+if __name__ == '__main__':
+    data = [random.randint(1, 5) for i in range(10)]
+    print data
+    print radix_sort(data)
+
+
+# """求数字的最高位"""
+# i = 9258900000
+# if i >= 100000000:
+#     i /= 100000000
+# if i >= 10000:
+#     i /= 10000
+# if i >= 100:
+#     i /= 100
+# if i >= 10:
+#     i /= 10
+# print i
+#
+# number = 839040000000000000000000
+# while number >= 10:
+#     number /= 10
+# print number
